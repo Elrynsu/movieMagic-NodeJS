46b3c3178079ff3800049003c7f711407dd8ced3
Teodor <elrynsier@gmail.com>
2025-01-18 21:48:24 +0200

    uuid installed and changed ids in movies

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,6 @@
 export default [
     {
-        id: '1',
+        id: 'cagb367f-b9e1-4a15-a767-af6410a75539',
         title: 'Jungle Cuise',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
@@ -11,7 +11,7 @@ export default [
         rating: '6.6'
     },
     {
-        id: '2',
+        id: 'seeb367f-b9e1-4a15-a767-af6410a75421',
         title: 'Tenet',
         genre: 'Thriller',
         description: 'Armed with only the word "Tenet," and fighting for the survival of the entire world, CIA operative, The Protagonist, journeys through a twilight world of international espionage on a global mission that unfolds beyond real time.',
