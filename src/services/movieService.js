65b01fb6b380c0b5554666661e749bcf6c0f44a7
Teodor <elrynsier@gmail.com>
2025-01-18 23:59:14 +0200

    added filter option for movies

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -20,8 +20,22 @@ const movieService = {
         
         return newId;
     },
-    getAll() {
-        return movies;
+    getAll(filter = {}) {
+        let result = movies;
+
+        if(filter.search) {
+            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
+        }
+
+        if(filter.genre) {
+            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase())
+        }
+
+        if(filter.year) {
+            result = result.filter(movie => movie.year === filter.year);
+        }
+
+        return result;
     }
 };
 
