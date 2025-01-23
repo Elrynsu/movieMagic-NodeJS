0878666e4469b74115818f9b32f00fb8a08c402f
Teodor <elrynsier@gmail.com>
2025-01-23 14:12:29 +0200

    mongoose setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import express from "express";
 import handlebars from 'express-handlebars';
+import mongoose from "mongoose";
+
 import routes from './routes.js';
 import showRatingHelper from "./helpers/ratingHelper.js";
 
 const app = express();
 
+//db configuration
+try {
+    const uri = 'mongodb://localhost:27017/magicMovies';
+    await mongoose.connect(uri);
+
+    console.log('DB connected successfully!');
+} catch(err) {
+    console.log('Couldn\'t connect to DB');
+    console.error(err.message);
+}
+
+//handlebars configuration
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
     helpers: {
@@ -14,9 +28,11 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+//express configuration
 app.use('/static', express.static('src/public')); //Load the static files from folder Public.
 app.use(express.urlencoded({ extended: false })); //Learn express to parse form data.
 
 app.use(routes); // All routes in routes file.
 
+//Server start
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
