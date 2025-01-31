16940fdd30a3d3085604109d4c351ad6f136fad2
Teodor <elrynsier@gmail.com>
2025-01-31 15:23:59 +0200

    add dynamic navigation

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -17,6 +17,7 @@ export const authMiddleware = (req, res, next) => {
 
         //Attach decoded token to request
         req.user = decodedToken;
+        res.locals.user = decodedToken;
 
         next();
     }catch(err) {
